69cec8d0fa1568aa888cfcea09f7a17626e052f9
HFarhad2843 <146637915+HFarhad2843@users.noreply.github.com>
2023-12-17 14:09:01 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static ConsoleApp9.test1;
+
+Console.WriteLine("Hello, World!");
+
+
+Test<string> test = new Test<string>();
+test.Add("Salam1");
+test.Add("Salam");
+test.Add("Salam1");
+test.Add("Salam");
+test.customRemove ("Salam");
+test.ShowAll();
